025f967140fdc651d56a80cd8deb1db0e640b5ff
Florjan96 <florianshiqerukaj@yahoo.com>
2022-06-29 14:45:59 +0200

    Update the email list

--- src/main.ts
+++ src/main.ts
@@ -33,3 +33,58 @@ const state = {
     // feel free to add more emails here
   ]
 }
+console.log(state)
+
+function emailList(){
+
+  
+// <h1>Inbox</h1>
+// <ul class="emails-list">
+//   <li class="emails-list__item">
+//     <span class="emails-list__item__read-icon material-symbols-outlined">
+//       mark_email_unread
+//     </span>
+//     <img class="emails-list__item__image" src="assets/nico.JPG" />
+//     <p class="emails-list__item__from">Nico</p>
+//     <p class="emails-list__item__content">
+//       Link to today's video and slides is up!
+//     </p>
+//   </li>
+// </ul>
+for (let email of state.emails){
+  let eList=document.querySelector(".email-list")
+
+let inbox=document.createElement('h1')
+inbox.textContent="Inbox"
+
+let emailListUl=document.createElement('ul')
+emailListUl.className="emails-list"
+
+let liItem=document.createElement('li')
+liItem.className="emails-list__item"
+
+ 
+let span=document.createElement('span')
+span.className="emails-list__item__read-icon material-symbols-outlined"
+span.textContent=" mark_email_unread"
+
+let img=document.createElement('img')
+img.className="emails-list__item__image"
+img.src="assets/nico.JPG" 
+
+let name=document.createElement('p')
+name.className="emails-list__item__from"
+name.textContent="Nico"
+
+let content=document.createElement('p')
+content.className="emails-list__item__content"
+content.textContent="Link to today's video and slides is up!"
+
+
+liItem.append(span,img,name,content)
+emailListUl.append(liItem)
+eList.append(emailListUl,inbox)
+
+}
+}
+emailList()
